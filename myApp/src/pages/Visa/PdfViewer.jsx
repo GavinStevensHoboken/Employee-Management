ce927535b6c93c2e99b8fd763b5c3f6b4dc13c95
Guangwei Na <gna@stevens.edu>
2024-01-21 21:37:12 -0500

    Add public pdfViewer component

---
+++ myApp/src/pages/Visa/PdfViewer.jsx
@@ -0,0 +1,31 @@
+// Pass base64String then render pdf
+import React from 'react';
+import { Viewer } from '@react-pdf-viewer/core';
+import {Worker} from '@react-pdf-viewer/core'
+
+import '@react-pdf-viewer/core/lib/styles/index.css';
+
+import { defaultLayoutPlugin } from '@react-pdf-viewer/default-layout';
+import '@react-pdf-viewer/default-layout/lib/styles/index.css';
+
+const PdfViewer = ({ base64String }) => {
+    const defaultLayoutPluginInstance = defaultLayoutPlugin();
+  return (
+    <div className="viewer">
+
+        {/* render this if we have a pdf file */}
+        {base64String&&(
+          <Worker workerUrl="https://unpkg.com/pdfjs-dist@2.16.105/build/pdf.worker.min.js">
+            <Viewer fileUrl={base64String}
+            plugins={[defaultLayoutPluginInstance]}></Viewer>
+          </Worker>
+        )}
+
+        {/* render this if we have pdfFile state null   */}
+        {!base64String&&<>No file is selected yet</>}
+
+      </div>
+  );
+};
+
+export default PdfViewer;
